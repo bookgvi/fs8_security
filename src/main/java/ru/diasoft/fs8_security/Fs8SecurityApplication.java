81e48ec24a19edf2695200327a0dd620f0818bb6
bookgvi <bookgvi@gmail.com>
2022-12-17 15:57:36 +0300

    git init

---
+++ src/main/java/ru/diasoft/fs8_security/Fs8SecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.diasoft.fs8_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fs8SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fs8SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/diasoft/fs8_security/Fs8SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.fs8_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs8SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
